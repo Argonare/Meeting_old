c45120a825f5d017da8a6c15628765dad4779516
hjy <584773506@qq.com>
2019-07-09 09:50:00 +0800

    船新版本

--- src/main/java/com/meeting/controller/UserController.java
+++ src/main/java/com/meeting/controller/UserController.java
@@ -1,9 +1,32 @@
 package com.meeting.controller;
 
+import com.meeting.bean.Msg;
+import com.meeting.bean.UserInfo;
+import com.meeting.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/userInfo")
 public class UserController {
+    @Autowired
+    UserInfoService userInfoService;
+
+    @ResponseBody
+    @RequestMapping(value = "/checkUser",method = RequestMethod.POST)
+    public Msg checkUser(UserInfo userInfo,HttpSession session){
+        String username = userInfo.getUsername();
+        String password = userInfo.getPassword();
+        if(userInfoService.checkUser(username,password)){
+            session.setAttribute("username",username);
+            return Msg.success();
+        }else{
+            return Msg.fail().add("msg","工号或密码错误");
+        }
+    }
 }
--- src/main/java/com/meeting/service/UserInfoService.java
+++ src/main/java/com/meeting/service/UserInfoService.java
@@ -1,7 +1,26 @@
 package com.meeting.service;
 
+import com.meeting.bean.UserInfo;
+import com.meeting.bean.UserInfoExample;
+import com.meeting.dao.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserInfoService {
+    @Autowired
+    UserInfoMapper userInfoMapper;
+    public boolean checkUser(String userid, String password) {
+        UserInfoExample userInfoExample = new UserInfoExample();
+        UserInfoExample.Criteria  criteria = userInfoExample.createCriteria();
+        criteria.andUsernameEqualTo(userid);
+        criteria.andPasswordEqualTo(password);
+        if(userInfoMapper.countByExample(userInfoExample) == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
