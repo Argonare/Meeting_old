ccde7a122b30f1a28ae473c792dbea1af8a4b4c0
hjy <584773506@qq.com>
2019-07-12 11:59:35 +0800

    7.12

--- src/main/java/com/meeting/controller/MeetingSigninController.java
+++ src/main/java/com/meeting/controller/MeetingSigninController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/meetingSignin")
-public class MeetingSgninController {
+public class MeetingSigninController {
     @Autowired
     MeetingSigninService meetingSigninService;
     @Autowired
